0271596198804d3278dcdfd16a2a0844d4fc1025
Joseph Ngo <joengo83@gmail.com>
2013-02-20 11:46:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techu::Application.config.secret_token = '9aeb77d22a3be1c29b2640d612cfbefda64a4edfecf092ee8fe15fb274caf530344333ebda2bc01c1f8152a40c67f332264438b3f1360406ac4b99d9f6f2bcd0'
